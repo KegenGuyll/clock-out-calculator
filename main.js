e2470c1b67aae54c9a917733d94650674790f380
guyllk <guyllkegen@gmail.com>
2018-07-12 12:50:50 -0500

    Time Machine V.02

--- main.js
+++ main.js
@@ -7,54 +7,53 @@ function Calculate() {
     //finds the clock-in time
     var clockINtime = document.getElementById("clock-in").value;
     console.log(clockINtime);
+
     //finds the lunch-out time
     var lunchOUTtime = document.getElementById("lunch-out").value;
     console.log(lunchOUTtime);
+
     //finds the lunch-in time
     var lunchINtime = document.getElementById("lunch-in").value;
     console.log(lunchINtime);
-    //finds the clock-out time
-    var clockOUTtime = document.getElementById("clock-out").value;
-    console.log(clockOUTtime);
-
-    //finds the time you spend at lunch
 
 
+    //takes the first two numbers from the string
+    var clockInHours = clockINtime.slice(0,2);
+    var clockInMin = clockINtime.slice(3,6);
+
+    var lunchOutHours = lunchOUTtime.slice(0,2);
+    var lunchOutMin = lunchOUTtime.slice(3,6);
+
+    var lunchInHours = lunchINtime.slice(0,2);
+    var lunchInMin = lunchINtime.slice(3,6);
 
 
-    //takes the hours from lunchout
-    var lunchOutHH = lunchOUTtime.slice(0,2);
-    console.log("lunchOUTHH:  = " + lunchOutHH);
+    //converts hours to min
+    var clockInMin1 = clockInHours * 60;
+    var lunchMin1 = lunchOutHours * 60;
+    var lunchMin2 = lunchInHours * 60;
 
-    var lunchMIN = lunchOutHH * 60;
+    // adds all the min together
+    var clockInMinTotal     = Number(clockInMin) + Number(clockInMin1);
+    var clockOutMealTotal   = Number(lunchOutMin) + Number(lunchMin1);
+    var clockInMealTotal    = Number(lunchInMin) + Number(lunchMin2);
 
-    var lunchOutMM = lunchOUTtime.slice(3,5);
-    console.log("lunchOUTMM:  = " + lunchOutMM);
+    var clockOutMin         = clockInMinTotal + 480;
+    var mealTime            = clockInMealTotal - clockOutMealTotal;
+    var clockOutMinFinal    = mealTime + clockOutMin;
 
-    lunchMIN += +lunchOutMM;
-    console.log("total min " + lunchMIN);
+    var clockOutHour = parseInt(clockOutMinFinal / 60);
+    var clockOutMin = clockOutMinFinal % 60;
+
+    var clockOutHourFinal = clockOutHour - 12;
+
+    var clockOutTime = clockOutHourFinal + ":" + clockOutMin;
+    console.log(clockOutTime);
+
+    document.getElementById("time").innerHTML = clockOutTime + " PM";
 
 
-    var lunchInHH = lunchINtime.slice(0,2);
-    console.log("lunchINHH:  = " + lunchInHH);
 
-    var lunchMINtemp = lunchInHH * 60;
 
-    var lunchInMM = lunchOUTtime.slice(3,5);
-    console.log("lunchINMM:  = " + lunchInMM);
-
-    lunchMINtemp -= +lunchInMM;
-    console.log("total min " + lunchMINtemp);
-
-    lunchMINtemp -= +lunchMIN;
-    console.log("total min " + lunchMIN);
-
-    //var lunchIN = moment(lunchINtime, "HH:mm");
-    //console.log("lunchOUTtime  = " + lunchOUT);
-    //console.log("lunchINtime  = " + lunchIN);
-
-    //console.log(JSON.stringify({ lunchOUTtime }));
-    //lunchtime = lunchOUT - lunchIN;
-    //console.log(lunchtime);
 
 }
